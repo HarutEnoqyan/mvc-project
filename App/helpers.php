9dae39748b88c637ab2a21cd0f4168edae7d0762
Harut Enoqyan <harut.enoqyan87@gmail.com>
2018-07-18 15:46:05 +0400

    added header and footer to view function

--- App/helpers.php
+++ App/helpers.php
@@ -5,6 +5,13 @@
  var_dump($arg);die();
 }
 
-function view() {
+function view($viewName , $params = []) {
 
+    require_once  BASE_PATH .'\\App\\Layoutes\\header.php';
+
+    if (file_exists($viewName) {
+        require_once BASE_PATH . '\\App\\Views\\'.$viewName.'.php'
+    });
+
+    require_once  BASE_PATH .'\\App\\Layoutes\\footer.php';
 }
