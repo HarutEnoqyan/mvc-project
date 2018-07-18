b74c7d72108d73fda94937a10587e34715b24d08
Harut Enoqyan <harut.enoqyan87@gmail.com>
2018-07-18 15:45:30 +0400

    removed header and footer from autoloader

--- autoload.php
+++ autoload.php
@@ -3,9 +3,12 @@
 define("BASE_PATH" , dirname(__FILE__));
 
 spl_autoload_register(function ($class) {
+
     $path =   BASE_PATH . DIRECTORY_SEPARATOR ."App" . DIRECTORY_SEPARATOR . $class .DIRECTORY_SEPARATOR . "$class.php";
 //    dd($path);
    require_once $path;
+
+
 });
 
 
