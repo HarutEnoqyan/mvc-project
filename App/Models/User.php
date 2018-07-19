072a27a3806156192af304e6c8c5cc8e3a91984c
Harut Enoqyan <harut.enoqyan87@gmail.com>
2018-07-19 16:53:55 +0400

    moving orm and model folder

--- App/Models/User.php
+++ App/Models/User.php
@@ -1,9 +1,13 @@
 <?php
- namespace Models;
+ namespace App\Models;
+ use Core\ORM;
 
- class User  {
+ class User  extends ORM {
+     protected $table = 'users';
+
      public function create() {
          global $pdh;
+
          $first_name = '"'. $_POST['first_name'].'"';
          $last_name = '"'.$_POST['last_name'].'"';
          $age = '"'.$_POST['age'].'"';
@@ -23,6 +27,23 @@
          } catch (\Exception $e) {
              echo 'Caught exception: ',  $e->getMessage(), "\n";
          }
+
+     }
+
+     public function login() {
+         global $pdh;
+
+         $email = '"'. $_POST['email'].'"';
+         $password = $_POST['password'];
+         $user = $this->where("email=$email")->get();
+
+         if($user && $user[0]->attributes['password']==$password) {
+             return $user;
+         } else  {
+             return ;
+         }
+
+
 
      }
  }
--- Models/ORM.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Models;
-
-class ORM {
-    public function
-}
