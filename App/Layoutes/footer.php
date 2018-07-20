b111736c7f5b37a10692bbd1ff49d61ff16816de
Harut Enoqyan <harut.enoqyan87@gmail.com>
2018-07-20 14:16:26 +0400

    for fontawesome

--- App/Layoutes/footer.php
+++ App/Layoutes/footer.php
@@ -1,5 +1,6 @@
 
-	<script type="text/javascript" src="js/main.js"></script>
-    <script type="text/javascript" src="js/bootstrap.min.js"</script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/fontawesome.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
 	</body>
 </html>
