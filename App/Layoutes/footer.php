a8b3b6b9a33b95f32dfd6939ee73f0f1d4fe1bae
Harut Enoqyan <harut.enoqyan87@gmail.com>
2018-07-24 16:13:52 +0400

    jquery and propper skripts

--- App/Layoutes/footer.php
+++ App/Layoutes/footer.php
@@ -1,6 +1,8 @@
 
-<!--    <script type="text/javascript" src="js/bootstrap.min.js"></script>-->
-    <script type="text/javascript" src="js/fontawesome.min.js"></script>
-    <script type="text/javascript" src="js/main.js"></script>
-	</body>
+<script type="text/javascript" src="https://unpkg.com/popper.js@1.14.3/dist/umd/popper.js"></script>
+<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/fontawesome.min.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+</body>
 </html>
