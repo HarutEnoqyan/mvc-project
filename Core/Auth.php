b55b78f81420c5bb40a80791dc306d23212b6b0d
Harut Enoqyan <harut.enoqyan87@gmail.com>
2018-07-25 11:06:22 +0400

    Auth getFullName functionality

--- Core/Auth.php
+++ Core/Auth.php
@@ -1,5 +1,6 @@
 <?php
 namespace Core;
+use App\Models\User;
 class Auth {
     public static function checkIfAuth()
     {
@@ -28,6 +29,23 @@ class Auth {
         if ($_SESSION && isset($_SESSION['id'])){
             return $_SESSION['id'];
         }
+    }
+
+    public static function getFullName() {
+        if (session_id()===''){
+            session_start();
+        }
+        if ($_SESSION && isset($_SESSION['id'])){
+            $id = $_SESSION['id'];
+        }
+
+        $user = new User();
+        $person =  $user->where("id = $id")->first();
+        $first_name = $person->attributes['first_name'];
+        $last_name = $person->attributes['last_name'];
+        return $first_name . ' ' . $last_name;
+
+
     }
 
 
